e1d32450b836d3fb8bd564c25f72af749b10dc79
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2024-09-25 01:47:35 +0200

    Inital commit

---
+++ src/contactpage.js
@@ -0,0 +1,22 @@
+// contactpage.js file
+
+function loadContact() {
+    const content = document.getElementById("content");
+    
+    content.innerHTML = "";
+    
+    const divText = document.createElement("div");
+    divText.setAttribute("id", "text");
+    content.appendChild(divText);
+
+    
+    const h1 = document.createElement("h1");
+    h1.textContent = "This is the Contact Page";
+    divText.appendChild(h1);
+
+    const p = document.createElement("p");
+    p.textContent = "Savor fresh fish, vibrant vegetables, and traditional dishes like Chicken Rendang, all made with local ingredients and love. Join us for a true taste of Lombok’s rich culinary heritage"
+    divText.appendChild(p);
+}
+
+export { loadContact }
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { loadMain } from "./mainpage";
+import { loadMenu } from "./menupage";
+import { loadContact } from "./contactpage";
+
+window.onLoad = loadMain();
+
+document.getElementById("menu-button").addEventListener("click", loadMenu)
+document.getElementById("home-tab").addEventListener("click", loadMain);
+document.getElementById("menu-tab").addEventListener("click", loadMenu);
+document.getElementById("contact-tab").addEventListener("click", loadContact);
